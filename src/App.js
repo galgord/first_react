e98ee7a3248cbd49c7b04d2853cf76db6ea78d69
Gal Gordon <gal.gordon@gmail.com>
2019-12-14 16:30:07 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import GalsForm from './GalsForm';
+function App() {
+  return (
+    <div className="App">
+    <GalsForm></GalsForm>
+  );
+}
+
+export default App;
---
+++ src/GalsForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class GalsForm extends React.Component{
+render(){
+return  <p>this is a p</p>;
+}
+}
+
+export default GalsForm;
